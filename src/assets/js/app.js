edda60403e96cfdcfd9efc630589e09789222392
rvitalia <rvitalia@mail.ru>
2024-04-25 04:08:05 +0300

    up

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -8,23 +8,27 @@ const swiper = new Swiper('.industries__swiper', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-    // breakpoints: {
-    //     // when window width is >= 320px
-    //     320: {
-    //       slidesPerView: 2,
-    //       spaceBetween: 20
-    //     },
-    //     // when window width is >= 480px
-    //     480: {
-    //       slidesPerView: 3,
-    //       spaceBetween: 30
-    //     },
-    //     // when window width is >= 640px
-    //     640: {
-    //       slidesPerView: 4,
-    //       spaceBetween: 40
-    //     }
-    //   }
+    breakpoints: {
+        // when window width is >= 320px
+        200: {
+          slidesPerView: 1,
+          spaceBetween: 10
+        },
+        576: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        // when window width is >= 480px
+        850: {
+          slidesPerView: 3,
+          spaceBetween: 40
+        },
+        // when window width is >= 640px
+        1240: {
+          slidesPerView: 4,
+          spaceBetween: 50
+        }
+      }
   });
 const swiperReviews = new Swiper('.swiper__reviews', {
     // Optional parameters
